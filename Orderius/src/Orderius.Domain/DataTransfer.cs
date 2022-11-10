32254b671036fe60b29e492c15cedc4742f7bcee
vnikitin761 <vladislovenikitin@gmail.com>
2022-11-10 23:39:47 +0300

    feat: added dto, changed order model

---
+++ Orderius/src/Orderius.Domain/DataTransfer.cs
@@ -0,0 +1,16 @@
+namespace Orderius.Domain;
+
+public struct DataTransfer
+{
+    private Dictionary<string,object> _properties;
+
+    public DataTransfer(Dictionary<string, object> properties = null)
+    {
+        _properties = properties ?? new Dictionary<string,object>();
+    }
+
+    public T? GetProperty<T>(string propertyName) =>
+        _properties[propertyName] is T ? (T)_properties[propertyName] : default;
+
+    public void SetProperty<T>(string propertyName, T propertyValue) => _properties.Add(propertyName,propertyValue);
+}
---
+++ Orderius/src/Orderius.Domain/PropertyType.cs
@@ -0,0 +1,9 @@
+namespace Orderius.Domain;
+
+public enum PropertyType
+{
+    Int = 1
+    ,String = 2
+    ,Datetime = 3
+    ,TypeUnit = 4
+}
